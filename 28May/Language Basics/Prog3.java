6c91744cbcf1fbdd0a92aafd62d047adcee660e5
Nikitha Keerthana <58261586+nikki-ofcl@users.noreply.github.com>
2020-05-28 20:40:47 +0530

    Create Prog3.java

---
+++ 28May/Language Basics/Prog3.java
@@ -0,0 +1,11 @@
+package wipro;
+
+public class Prog3 {
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = a + b;
+		
+		System.out.printf("The sum of %d and %d is %d", a, b, c);
+	}
+}
