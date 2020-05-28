f3b9189ba291f80be315c52d0be41f563fb0305b
Nikitha Keerthana <58261586+nikki-ofcl@users.noreply.github.com>
2020-05-28 22:25:46 +0530

    Create Array6.java

---
+++ 28May/Arrays/Array6.java
@@ -0,0 +1,9 @@
+package wipro;
+import java.util.Arrays;
+public class Array6 {
+	public static void main(String[] args) {
+		int[] array = {48, 55, 68, 88, 101, 122};
+		Arrays.sort(array);
+		System.out.println(Arrays.toString(array));
+	}
+}
