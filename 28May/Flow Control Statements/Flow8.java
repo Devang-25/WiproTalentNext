e20d6eeaea774cd7ca3c851fe44dee19445ec23a
Nikitha Keerthana <58261586+nikki-ofcl@users.noreply.github.com>
2020-05-28 21:19:41 +0530

    Create Flow8.java

---
+++ 28May/Flow Control Statements/Flow8.java
@@ -0,0 +1,30 @@
+package wipro;
+import java.util.*;
+public class Flow8 {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			char s = sc.next().charAt(0);
+			switch(s) {
+			  case 'R':
+			    System.out.println("Red");
+			    break;
+			  case 'B':
+				System.out.println("Blue");
+			    break;
+			  case 'G':
+			    System.out.println("Green");
+				break;
+			  case 'O':
+				System.out.println("Orange");
+				break;
+			  case 'Y':
+				System.out.println("Yellow");
+				break;
+			  case 'W':
+				System.out.println("White");
+				break;
+			  default:
+			    System.out.println("Invalid Code");
+			}
+		}
+}
