d508efa5d4d44083cc5ff86a3f5876725a7ff864
Nikitha Keerthana <58261586+nikki-ofcl@users.noreply.github.com>
2020-05-28 22:16:14 +0530

    Create Array10.java

---
+++ 28May/Arrays/Array10.java
@@ -0,0 +1,34 @@
+package wipro;
+public class Array10 {
+	public static void main(String[] args) {
+		if (args.length != 4)
+			System.out.println("Please enter 4 integer numbers");
+		int[][] array = new int[2][2];
+		int[][] arrayReversed = new int[2][2];
+		int x = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[0].length; j++) {
+				array[i][j] = Integer.parseInt(args[x++]);
+			}
+		}
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[0].length; j++) {
+				arrayReversed[array.length - 1 - i][array.length - 1 - j] = array[i][j];
+			}
+		}
+		System.out.println("The given array is :");
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[0].length; j++) {
+				System.out.print(array[i][j] + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println("The reverse of the array is :");
+		for (int i = 0; i < arrayReversed.length; i++) {
+			for (int j = 0; j < arrayReversed[0].length; j++) {
+				System.out.print(arrayReversed[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+}
