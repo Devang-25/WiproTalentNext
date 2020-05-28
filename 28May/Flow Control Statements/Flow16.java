403da5d9be2cd741abbb3669692e546dae138afa
Nikitha Keerthana <58261586+nikki-ofcl@users.noreply.github.com>
2020-05-28 21:40:18 +0530

    Create Flow16.java

---
+++ 28May/Flow Control Statements/Flow16.java
@@ -0,0 +1,16 @@
+package wipro;
+public class Flow16 {
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Please enter an integer number");
+			System.exit(0);
+		}
+		int rowCount = Integer.parseInt(args[0]);	
+		for (int i = 0; i < rowCount; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
