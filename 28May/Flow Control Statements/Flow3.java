ed1bdef36a7a61e70bed94ce6078ab4dbcde369e
Nikitha Keerthana <58261586+nikki-ofcl@users.noreply.github.com>
2020-05-28 20:54:49 +0530

    Create Flow3.java

---
+++ 28May/Flow Control Statements/Flow3.java
@@ -0,0 +1,15 @@
+package wipro;
+
+public class Flow3 {
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("No Values");
+		} 
+		else {
+			for (int i = 0; i < args.length; i++) {
+				System.out.print(args[i]);
+				if (i < args.length - 1) System.out.print(", ");
+			}
+		}
+	}
+}
