7e60f2c68a6c1e8c9db47d24ed3a20c30aa61d10
Nikitha Keerthana <58261586+nikki-ofcl@users.noreply.github.com>
2020-05-28 21:48:43 +0530

    Create Flow19.java

---
+++ 28May/Flow Control Statements/Flow19.java
@@ -0,0 +1,14 @@
+package wipro;
+public class Flow19 {
+public static void main(String[] args) {
+		int counter = 0;
+		int i = 0;
+		while (counter != 5) {
+			i++;
+			if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+				System.out.println(i);
+				counter++;
+			}
+		}
+	}
+}
