ae44acd72a444185812cf7403014b79fcbc28a03
Nikitha Keerthana <58261586+nikki-ofcl@users.noreply.github.com>
2020-05-28 22:28:54 +0530

    Create Array4.java

---
+++ 28May/Arrays/Array4.java
@@ -0,0 +1,10 @@
+package wipro;
+
+public class Array4 {
+	public static void main(String[] args) {
+		int[] array = {48, 55, 68, 88, 101, 122};
+		for (int i = 0; i < array.length; i++) {
+			System.out.printf("%c ", array[i]);
+		}
+	}
+}
