1db5b98bfe786dde9ff752443aa3dc39aa811330
Nikitha Keerthana <58261586+nikki-ofcl@users.noreply.github.com>
2020-05-29 19:42:29 +0530

    Create Person.java

---
+++ 29May/Inheritance/In2/Person.java
@@ -0,0 +1,14 @@
+package wipro;
+public class Person {
+	protected String name;
+	public Person(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + "]";
+	}
+}
