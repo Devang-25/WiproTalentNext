e04d554d18033b154300e8f495c93bf31d7f48f7
Nikitha Keerthana <58261586+nikki-ofcl@users.noreply.github.com>
2020-05-29 19:40:17 +0530

    Create TestEmployee.java

---
+++ 29May/Inheritance/In2/TestEmployee.java
@@ -0,0 +1,8 @@
+package wipro;
+import wipro.Employee;
+public class TestEmployee {
+	public static void main(String[] args) {
+		Employee employee = new Employee("John Doe", 5000000, 2019, "ABC506892");
+		System.out.println(employee);
+	}
+}
