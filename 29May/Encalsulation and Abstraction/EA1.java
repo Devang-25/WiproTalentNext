a846a59a502d84b26cd1a52eb2399aa7ecc20a7d
Nikitha Keerthana <58261586+nikki-ofcl@users.noreply.github.com>
2020-05-29 19:25:00 +0530

    Create EA1.java

---
+++ 29May/Encalsulation and Abstraction/EA1.java
@@ -0,0 +1,68 @@
+package wipro;
+import java.util.Scanner;
+class Author {
+	private String name;
+	private String email;
+	private char gender;
+	public Author(String name, String email, char gender) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.gender = gender;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public char getGender() {
+		return gender;
+	}
+	@Override
+	public String toString() {
+		return "Author [name=" + name + ", email=" + email + ", gender=" + gender + "]";
+	}
+}
+class Book {
+	private String name;
+	private Author author;
+	private double price;
+	private int qtyInStock;
+	public Book(String name, Author author, double price, int qtyInStock) {
+		super();
+		this.name = name;
+		this.author = author;
+		this.price = price;
+		this.qtyInStock = qtyInStock;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQtyInStock() {
+		return qtyInStock;
+	}
+	public void setQtyInStock(int qtyInStock) {
+		this.qtyInStock = qtyInStock;
+	}
+	public String getName() {
+		return name;
+	}
+	public Author getAuthor() {
+		return author;
+	}
+	@Override
+	public String toString() {
+		return "Book [name=" + name + ", author=" + author + ", price=" + price + ", qtyInStock=" + qtyInStock + "]";
+	}
+}	
+public class EA1 {
+	public static void main(String[] args) {
+		Author author = new Author("John Doe", "johndoe@email.com", 'M');
+		Book book = new Book("Java", author, 1500, 5);
+		System.out.println(book);
+	}
+}
